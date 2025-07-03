1d8dd9d41feccf477f1506626f27cb767666c2c4
Sergey Matyukevich <geomatsi@gmail.com>
2025-07-03 23:34:19 +0300

    conftest: add repeat option to repeat test
    Add '--repeat' option to repeat selected tests.
    Use the approach suggested on stackoverflow:
    - https://stackoverflow.com/questions/21764473/how-can-i-repeat-each-test-multiple-times-in-a-py-test-run
    
    Signed-off-by: Sergey Matyukevich <geomatsi@gmail.com>

--- lab/conftest.py
+++ lab/conftest.py
@@ -31,3 +31,17 @@ def restart_shell(strategy, capsys):
 @pytest.fixture(scope="function")
 def assume_shell(strategy, capsys):
     pass
+
+
+# options
+
+
+def pytest_addoption(parser):
+    parser.addoption('--repeat', action='store', help='Repeat test the specified number of times')
+
+
+def pytest_generate_tests(metafunc):
+    if metafunc.config.option.repeat is not None:
+        count = int(metafunc.config.option.repeat)
+        metafunc.fixturenames.append('tmp_ct')
+        metafunc.parametrize('tmp_ct', range(count))
