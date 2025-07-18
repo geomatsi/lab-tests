6a5a76e55e08684e95c83dd301e5744d70404282
Sergey Matyukevich <geomatsi@gmail.com>
2025-07-18 23:45:09 +0300

    test_uboot: add simple ping test
    Signed-off-by: Sergey Matyukevich <geomatsi@gmail.com>

--- lab/tests/test_uboot.py
+++ lab/tests/test_uboot.py
@@ -1,8 +1,24 @@
-def test_uboot_version(target, in_uboot):
+from utils import mgmt
+
+
+def test_version(target, in_uboot):
     command = target.get_driver('UBootDriver')
-    stdout, stderr, returncode = command.run("version")
-    assert returncode == 0
+    stdout, stderr, ret = command.run("version")
+    assert ret == 0
     assert stdout
     assert not stderr
     # check uboot version output
     assert 'U-Boot' in stdout[0]
+
+
+def test_ping(target, in_uboot):
+    strategy = target.get_strategy()
+    command = target.get_driver('UBootDriver')
+    _, _, ret = command.run(f"setenv serverip {mgmt.MGMT_SERVER_ADDR}")
+    assert ret == 0
+    _, _, ret = command.run(f"setenv ipaddr {mgmt.MGMT_TEST_ADDR}")
+    assert ret == 0
+    _, _, ret = command.run(f"ping {mgmt.MGMT_SERVER_ADDR}")
+    assert ret == 0
+
+    strategy.transition("off")
