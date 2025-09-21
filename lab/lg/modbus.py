bd115d39c8d0236054151923c048aed06633adf5
Sergey Matyukevich <geomatsi@gmail.com>
2025-09-21 23:37:19 +0300

    modbus: connect to server only when needed
    Currently connection to Modbus TCP server is opened on activation and
    closed on deactivation. That means that connection is kept alive during
    the time of the test session. This does not work well if multi-channel
    relay board supports only limited number of opened connection. For
    instance, this is the case for DTWonder relay board: it supports only
    two simultaneous open connections.
    
    In fact no need to keep connection to Modbus TCP opened all the time.
    This change modifies ModbusTCPPowerDriver to open connection only for
    each requested power operation and close right after its completion.
    
    Signed-off-by: Sergey Matyukevich <geomatsi@gmail.com>

--- lab/lg/modbus.py
+++ lab/lg/modbus.py
@@ -2,6 +2,7 @@
 
 from importlib import import_module
 
+import functools
 import socket
 import time
 import attr
@@ -21,22 +22,55 @@ class ModbusTCPPowerDriver(Driver, PowerProtocol):
     downtime = attr.ib(default=1.0, validator=attr.validators.instance_of(float))
     delay = attr.ib(default=0.5, validator=attr.validators.instance_of(float))
 
+    retry_delay = attr.ib(default=1.0, validator=attr.validators.instance_of(float))
+    retries = attr.ib(default=3, validator=attr.validators.instance_of(int))
+
     def __attrs_post_init__(self):
         super().__attrs_post_init__()
         self._modbus = import_module('umodbus.client.tcp')
         self.sock = None
+        self.port = 0
+        self.host = 0
+
+    def modbus_connect(func):
+        @functools.wraps(func)
+        def wrapper(self, *args, **kwargs):
+            # try to connect to Modbus TCP server with retries
+            sock = None
+            for t in range(1, self.retries + 1):
+                try:
+                    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                    sock.connect((self.host, int(self.port)))
+                    break
+                except OSError as e:
+                    sock.close()
+
+                    if t < self.retries:
+                        time.sleep(self.retry_delay)
+                    else:
+                        raise e
+
+            try:
+                self.sock = sock
+                # ready to go: perform the requested power mgmt operation
+                func(self, *args, **kwargs)
+            finally:
+                self.sock.close()
+                self.sock = None
+
+        return wrapper
 
     def on_activate(self):
         # we can only forward if the backend knows which port to use
-        host, port = proxymanager.get_host_and_port(self.coil, default_port=502)
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.connect((host, int(port)))
+        self.host, self.port = proxymanager.get_host_and_port(self.coil, default_port=502)
 
     def on_deactivate(self):
-        self.sock.close()
+        if self.sock:
+            self.sock.close()
 
     @Driver.check_active
     @step()
+    @modbus_connect
     def on(self):
         value = (((1 << self.coil.coil) << 8) | (1 << self.coil.coil))
         req = self._modbus.write_single_register(slave_id=1, address=2, value=value)
@@ -45,6 +79,7 @@ class ModbusTCPPowerDriver(Driver, PowerProtocol):
 
     @Driver.check_active
     @step()
+    @modbus_connect
     def off(self):
         value = ((1 << self.coil.coil) << 8)
         req = self._modbus.write_single_register(slave_id=1, address=2, value=value)
@@ -53,14 +88,17 @@ class ModbusTCPPowerDriver(Driver, PowerProtocol):
 
     @Driver.check_active
     @step()
+    @modbus_connect
+    def get(self):
+        req = self._modbus.read_holding_registers(slave_id=1, starting_address=1, quantity=1)
+        rsp = self._modbus.send_message(req, self.sock)
+        return int(rsp[0]) & (1 << self.coil.coil)
+
+    @Driver.check_active
+    @step()
     def cycle(self):
         self.off()
         time.sleep(self.downtime)
         self.on()
 
-    @Driver.check_active
-    @step()
-    def get(self):
-        req = self._modbus.read_holding_registers(slave_id=1, starting_address=1, quantity=1)
-        rsp = self._modbus.send_message(req, self.sock)
-        return int(rsp[0]) & (1 << self.coil.coil)
+
